94a5bc1d315e298cc946cd7d33fe24f162860f62
Benjamin Dreux <benjiiiiii@gmail.com>
2012-03-25 01:34:22 +0000

    remove theme

--- script/main.js
+++ script/main.js
@@ -3,7 +3,10 @@ require(["lib/jquery","lib/jquery-ui","lib/jquery-ui-spinner"], function(){
         $(".body").load(pageName+".html", then);    
     };
     var addSpinner = function(){
-        $('.spinner').spinner({ min: 0, max: 100 });
+        $(".spinner").attr("value",0);
+        $(".spinner").attr("min",0);
+        $(".spinner").attr("max",100);
+        $(".spinner").attr("width",20);
     };
     
     $().ready(function(){
